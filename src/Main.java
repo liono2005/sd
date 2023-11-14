c5b1034d4b9d4095f99add7daf4f19b1c710f9a9
liono2005 <132668154+liono2005@users.noreply.github.com>
2023-11-14 19:48:31 +0300

    DSFSD

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
-    public static void main(String[] args) {
+    public static void n(String[] args) {//GFHFXH
 
         Generic<Integer, String, Boolean> example = new Generic<>(10, "Hello", true);
         System.out.println(example.getParameter1());
